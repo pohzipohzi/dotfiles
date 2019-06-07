1c47e5aa8915dc7c2f93efed2fef62af1323b80f
Zi How Poh <z@pzpz.dev>
2019-06-08 05:35:45 +0800

    add docker commands

--- dotfiles/.scripts/pg/i.sh
+++ dotfiles/.scripts/pg/i.sh
@@ -1,2 +1,2 @@
-sudo -u postgres psql -c "CREATE TABLE IF NOT EXISTS bins (id bigint);"
-sudo -u postgres psql -c "INSERT INTO bins (id) SELECT g.id FROM generate_series(1, $1) AS g (id);"
+psql -h localhost -U postgres -d postgres -c "CREATE TABLE IF NOT EXISTS bins (id bigint);"
+psql -h localhost -U postgres -d postgres -c "INSERT INTO bins (id) SELECT g.id FROM generate_series(1, $1) AS g (id);"
