497c3e55bb71a559ea007c0c22d1b7c6af53f061
Zi How Poh <z@pzpz.dev>
2019-07-02 04:32:46 +0800

    remove sample pg data

--- dotfiles/.scripts/wg/reset.sh
+++ dotfiles/.scripts/wg/reset.sh
@@ -11,10 +11,10 @@ sed -i '' 's/#archive_mode = off/archive_mode = on/g' $PGDATA/postgresql.conf
 sed -i '' "s/#archive_command = ''/archive_command = 'wal-g wal-push %p'/g" $PGDATA/postgresql.conf
 pg_ctl -D $PGDATA start
 createdb $USER
+pgbench -i -s 1 $USER
 psql -h localhost -c "
 CREATE TABLESPACE tblspc LOCATION '$HOME/Desktop/postgres/tblspc';
 "
-psql -f $(dirname "$0")/dvdrental/restore.sql
 psql -h localhost -c "
 SET default_tablespace = tblspc;
 CREATE TABLE IF NOT EXISTS bins (id bigint);
