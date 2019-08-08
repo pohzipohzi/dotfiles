a182bfaf1046caebb40524065acd8fc5d570a3d0
Zi How Poh <z@pzpz.dev>
2019-08-09 02:01:44 +0800

    move whitefox config to within script

--- dotfiles/.scripts/wf/d.sh
+++ dotfiles/.scripts/wf/d.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+P=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    sudo dfu-util -D $DOTFILES/.whitefox/linux.bin
+    sudo dfu-util -D $P/config/linux.bin
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    sudo dfu-util -D $DOTFILES/.whitefox/mac.bin
+    sudo dfu-util -D $P/config/mac.bin
 fi
