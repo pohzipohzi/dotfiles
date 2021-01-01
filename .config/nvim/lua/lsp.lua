99db7799a7f8a935c511f1e03234b9bad07c4a1c
Zi How Poh <z@pzpz.dev>
2021-01-01 14:08:24 +0800

    add vim global to sumneko_lua setup

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -7,7 +7,15 @@ lsp.solargraph.setup{}
 lsp.tsserver.setup{}
 lsp.yamlls.setup{}
 lsp.jsonls.setup{}
-lsp.sumneko_lua.setup{}
+lsp.sumneko_lua.setup{
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
+}
 lsp.vimls.setup{}
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
