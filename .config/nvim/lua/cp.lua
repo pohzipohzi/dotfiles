faae5d6f5803137c9929fdf1ac1f9e69ca1e5e83
Zi How Poh <z@pzpz.dev>
2021-03-14 23:43:30 +0800

    add c cp mappings

--- .config/nvim/lua/cp.lua
+++ .config/nvim/lua/cp.lua
@@ -1,29 +1,30 @@
 -- shortcuts
-vim.api.nvim_command('autocmd FileType go nnoremap <buffer> <Leader>zx :lua RunCase()<CR>')
-vim.api.nvim_command('autocmd FileType go nnoremap <buffer> <Leader>zc :lua RunCases()<CR>')
-vim.api.nvim_command('autocmd FileType go nnoremap <buffer> <Leader>zi :lua RunCaseInteractive()<CR>')
+vim.api.nvim_command('autocmd FileType go nnoremap <buffer> <Leader>zc :lua RunTerm(RunCasesGo())<CR>')
+vim.api.nvim_command('autocmd FileType go nnoremap <buffer> <Leader>zi :lua RunTerm(RunInteractiveGo())<CR>')
+vim.api.nvim_command('autocmd FileType c nnoremap <buffer> <Leader>zc :lua RunTerm(RunCasesC())<CR>')
 vim.api.nvim_set_keymap('n', '<Leader>zo', ':tabe $HOME/dotfiles/.scripts/cp/<CR>', { noremap = true })
 
-function RunCase()
-  local prog = vim.fn.expand('%')
-  local input = vim.fn.expand('%:h') .. '/in'
-  RunTerm(string.format('go run %s < %s', prog, input))
-end
-
-function RunCases()
+function RunCasesGo()
   local prog = vim.fn.expand('%')
   local build = vim.fn.expand('%:p:r')
   local input = vim.fn.expand('%:h') .. '/in'
-  RunTerm(string.format('go build -o %s %s && piper -o -c %s < %s', build, prog, build, input))
+  return string.format('go build -o %s %s && piper -o -c %s < %s', build, prog, build, input)
 end
 
-function RunCaseInteractive()
+function RunInteractiveGo()
   local prog = vim.fn.expand('%')
-  RunTerm(string.format('go run %s', prog))
+  return string.format('go run %s', prog)
+end
+
+function RunCasesC()
+  local prog = vim.fn.expand('%')
+  local build = vim.fn.expand('%:h') .. '/a.out'
+  local input = vim.fn.expand('%:h') .. '/in'
+  return string.format('gcc %s && piper -o -c %s < %s', prog, build, input)
 end
 
 -- layout
-vim.api.nvim_command('e sol.go')
+vim.api.nvim_command('e')
 vim.api.nvim_command('vsplit out')
 vim.api.nvim_command('split in')
 vim.api.nvim_command('vert 3resize ' .. (vim.wo.colorcolumn + vim.wo.numberwidth + 4))
