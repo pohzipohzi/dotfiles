4e461ce301001d90f3a6e10d69127bbb7dfc195c
Zi How Poh <z@pzpz.dev>
2021-03-28 03:18:50 +0800

    refactor cp.lua, switch over lua language table and use nvim_buf_set_keymap api

--- .config/nvim/lua/cp.lua
+++ .config/nvim/lua/cp.lua
@@ -1,30 +1,28 @@
 -- shortcuts
-vim.api.nvim_command('autocmd FileType go nnoremap <buffer> <Leader>zc :lua RunTerm(RunCasesGo())<CR>')
-vim.api.nvim_command('autocmd FileType go nnoremap <buffer> <Leader>zi :lua RunTerm(RunInteractiveGo())<CR>')
-vim.api.nvim_command('autocmd FileType c nnoremap <buffer> <Leader>zc :lua RunTerm(RunCasesC())<CR>')
+local switch = {
+  ['go'] = function()
+    local prog = vim.fn.expand('%')
+    local build = vim.fn.expand('%:p:r')
+    local input = vim.fn.expand('%:h') .. '/in'
+    local cases_cmd = string.format('go build -o %s %s && piper -o -c %s < %s', build, prog, build, input)
+    local interactive_cmd = string.format('go run %s', prog)
+    return cases_cmd, interactive_cmd
+  end,
+  ['c'] = function()
+    local prog = vim.fn.expand('%')
+    local build = vim.fn.expand('%:h') .. '/a.out'
+    local input = vim.fn.expand('%:h') .. '/in'
+    local cases_cmd = string.format('gcc %s && piper -o -c %s < %s', prog, build, input)
+    local interactive_cmd = string.format('gcc %s && %s', prog, build)
+    return cases_cmd, interactive_cmd
+  end,
+}
+cases_cmd, interactive_cmd = switch[vim.bo.filetype]()
+vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>zc', string.format(':lua RunTerm("%s")<CR>', cases_cmd), { noremap=true })
+vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>zi', string.format(':lua RunTerm("%s")<CR>', interactive_cmd), { noremap=true })
 vim.api.nvim_set_keymap('n', '<Leader>zo', ':tabe $HOME/dotfiles/.scripts/cp/<CR>', { noremap = true })
 
-function RunCasesGo()
-  local prog = vim.fn.expand('%')
-  local build = vim.fn.expand('%:p:r')
-  local input = vim.fn.expand('%:h') .. '/in'
-  return string.format('go build -o %s %s && piper -o -c %s < %s', build, prog, build, input)
-end
-
-function RunInteractiveGo()
-  local prog = vim.fn.expand('%')
-  return string.format('go run %s', prog)
-end
-
-function RunCasesC()
-  local prog = vim.fn.expand('%')
-  local build = vim.fn.expand('%:h') .. '/a.out'
-  local input = vim.fn.expand('%:h') .. '/in'
-  return string.format('gcc %s && piper -o -c %s < %s', prog, build, input)
-end
-
 -- layout
-vim.api.nvim_command('e')
 vim.api.nvim_command('vsplit out')
 vim.api.nvim_command('split in')
 vim.api.nvim_command('vert 3resize ' .. (vim.wo.colorcolumn + vim.wo.numberwidth + 4))
