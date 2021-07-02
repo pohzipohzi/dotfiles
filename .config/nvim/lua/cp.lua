0086e7f2fb562f027906b89a5f6d3fa63244728c
Zi How Poh <z@pzpz.dev>
2021-07-03 04:19:33 +0800

    add silent opt for long term commands

--- .config/nvim/lua/cp.lua
+++ .config/nvim/lua/cp.lua
@@ -3,8 +3,8 @@ local build = vim.fn.expand('%:p:r')
 local input = vim.fn.expand('%:h') .. '/in'
 local cases_cmd = string.format('go build -o %s %s && piper -o -c %s < %s', build, prog, build, input)
 local interactive_cmd = string.format('go run %s', prog)
-vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>zc', string.format(':lua RunTerm("%s")<CR>', cases_cmd), { noremap=true })
-vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>zi', string.format(':lua RunTerm("%s")<CR>', interactive_cmd), { noremap=true })
+vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>zc', string.format(':lua RunTerm("%s")<CR>', cases_cmd), { noremap=true, silent=true })
+vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>zi', string.format(':lua RunTerm("%s")<CR>', interactive_cmd), { noremap=true, silent=true })
 vim.api.nvim_set_keymap('n', '<Leader>zo', ':tabe $HOME/dotfiles/.scripts/cp/<CR>', { noremap = true })
 
 -- layout
